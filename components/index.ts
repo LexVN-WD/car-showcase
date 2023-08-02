d4436e41060b693ea0eb340f3fe7a55a30a456fa
Lex Van-Nugent <lexvn.wd@gmail.com>
2023-08-02 17:00:03 -0600

    Import SearchManufacturer component

--- components/index.ts
+++ components/index.ts
@@ -6,12 +6,14 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { 
+export {
   Hero,
   CustomButton,
   Navbar,
   Footer,
   SearchBar,
-  CustomFilter
+  CustomFilter,
+  SearchManufacturer,
 };
