90ab0586a2bd1ed673b679455b1e4d4404351154
Lex Van-Nugent <lexvn.wd@gmail.com>
2023-08-02 20:40:21 -0600

    Add new router push search functionality

--- utils/index.ts
+++ utils/index.ts
@@ -1,18 +1,19 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
 
   const headers = {
-      "X-RapidAPI-Key": "7337061d36msh34b082da0f8da24p14bc7ajsn3bce08391378",
-      "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-  }
+    "X-RapidAPI-Key": "7337061d36msh34b082da0f8da24p14bc7ajsn3bce08391378",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
 
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3", {
-    
-    headers: headers,
-   
-  });
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    {
+      headers: headers,
+    }
+  );
 
   const result = await response.json();
 
@@ -34,7 +35,6 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   return rentalRatePerDay.toFixed(0);
 };
 
-
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
   const { make, model, year } = car;
@@ -44,8 +44,7 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   url.searchParams.append("modelFamily", model.split(" ")[0]);
   url.searchParams.append("zoomType", "fullscreen");
   url.searchParams.append("modelYear", `${year}`);
-  // url.searchParams.append('zoomLevel', zoomLevel);
   url.searchParams.append("angle", `${angle}`);
 
   return `${url}`;
-}; 
+};
