93355b1193b3151dd32c4b075ba348e4816a8f1b
Lex Van-Nugent <lexvn.wd@gmail.com>
2023-08-02 18:27:48 -0600

    Add initial CarCard code and logic

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+"use client";
 
-const CarCard = () => {
+import { useState } from 'react'
+import Image from 'next/image';
+import { CarProps } from '@/types';
+import { CustomButton } from "@/components";
+import { calculateCarRent } from '@/utils';
+
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+
+  const carRent = calculateCarRent(city_mpg, year);
+
   return (
-    <div>CarCard</div>
+    <div className='car-card group'>
+      <div className='car-card__content'>
+        <h2 className='car-card__content-title'>
+          {make} {model}
+        </h2>
+      </div>
+
+      <p className='flex mt-6 text-[32px] font-extrabold'>
+        <span className='self-start text-[14px]'>
+          $
+        </span>
+        {carRent}
+        <span className='self-end text-[14px] font-medium'>
+          /day
+        </span>
+      </p>
+    </div>
   )
 }
 
